98a3ece0d78ca71f85507c2163c112a52cbf01c8
Nour <ckastoun20@stuy.edu>
2020-11-01 21:54:40 -0500

    completed and no compile errors

--- Triangle.java
+++ Triangle.java
@@ -39,6 +39,17 @@ public class Triangle{
     return ("v1("+v1.getX()+", "+v1.getY()+") v2(" + v2.getX()+", "+v2.getY()+") v3(" +v3.getX()+", "+v3.getY()+")");
   }
 
+//mutator
+  public void setVertex(int index, Point newP){
+    if (index==0){
+      v1=newP;
+    }else if (index==1){
+      v2=newP;
+    }else{
+      v3=newP;
+    }
+  }
+
   public static void main(String[] args){
   }
 }
