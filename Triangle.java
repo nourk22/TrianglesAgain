5db12d57c1b24129f11ebf27c8e1d9d0914e99a7
Nour <ckastoun20@stuy.edu>
2020-11-01 21:41:38 -0500

    up thru 2.4c and no compile errors

--- Triangle.java
+++ Triangle.java
@@ -1,6 +1,7 @@
 public class Triangle{
   private Point v1, v2, v3;
 
+// constructors
   public Triangle(Point a, Point b, Point c){
     v1=a;
     v2=b;
@@ -12,7 +13,31 @@ public class Triangle{
     v2=new Point(x2, y2);
     v3=new Point(x3, y3);
   }
-  
+
+//accessors
+  public double getPerimeter(){
+    return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
+  }
+
+  public double getArea(){
+    double semi=getPerimeter()/(double)2;
+    double area=Math.sqrt(semi*(semi-v1.distanceTo(v2))*(semi-v2.distanceTo(v3))*(semi-v3.distanceTo(v1)));
+    return area;
+  }
+
+  public String classify(){
+    if (v1.distanceTo(v2)==v2.distanceTo(v3) && v1.distanceTo(v2)==v1.distanceTo(v3)){
+      return "equilateral";
+    }
+    if (v1.distanceTo(v2)==v2.distanceTo(v3) || v1.distanceTo(v2)==v1.distanceTo(v3) || v2.distanceTo(v3)==v1.distanceTo(v3)){
+      return "isosceles";
+    }
+    return "scalene";
+  }
+
+  public String toString(){
+    return ("v1("+v1.getX()+", "+v1.getY()+") v2(" + v2.getX()+", "+v2.getY()+") v3(" +v3.getX()+", "+v3.getY()+")");
+  }
 
   public static void main(String[] args){
   }
