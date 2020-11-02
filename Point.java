f78436102ec0095e586996439bccc47aa43fa491
Nour <ckastoun20@stuy.edu>
2020-11-01 19:30:56 -0500

    created point given coordinates

---
+++ Point.java
@@ -0,0 +1,14 @@
+public class Point{
+  private double x;
+  private double y;
+
+//construct a point given coordinates
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  public static void main(String[] args){
+    //tests are in a separate class
+  }
+}
