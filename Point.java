2bf736f40bd8a4ca527886723dad8e3779a037bb
Nour <ckastoun20@stuy.edu>
2020-11-01 19:43:27 -0500

    added everything thru equals (2.3b)

--- Point.java
+++ Point.java
@@ -8,7 +8,30 @@ public class Point{
     y=Y;
   }
 
+//construct a point given a point (redundant, just provides flexibility)
+  public Point(Point p){
+    x=p.x;
+    y=p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+//return distance from this point to the other point
+  public double distanceTo(Point other){
+    return (Math.sqrt(Math.pow(x-other.x, 2) + Math.pow(y-other.y, 2)));
+  }
+
+//returns true when this point has the same x andy values as the other point
+  public boolean equals(Point other){
+    return (other.x==x && other.y==y);
+  }
+
   public static void main(String[] args){
-    //tests are in a separate class
   }
 }
